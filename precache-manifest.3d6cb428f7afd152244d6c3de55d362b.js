1b208a9dd25ae875217f505b8afb45cb65145d9a
KANIN SRIJUNDORN <kaninsrijundorn@MacBook-Pro.local>
2020-07-08 16:43:31 +0700

    Updates

---
+++ precache-manifest.3d6cb428f7afd152244d6c3de55d362b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805250798df3f94d8c01fd748387db5f",
+    "url": "/googleAuth/index.html"
+  },
+  {
+    "revision": "fa4f440ffb3da9708cd0",
+    "url": "/googleAuth/static/css/main.d1e463b3.chunk.css"
+  },
+  {
+    "revision": "03c608269aeacfb8fc23",
+    "url": "/googleAuth/static/js/2.8016234f.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/googleAuth/static/js/2.8016234f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa4f440ffb3da9708cd0",
+    "url": "/googleAuth/static/js/main.8fd20555.chunk.js"
+  },
+  {
+    "revision": "d1019ad146403cff750f",
+    "url": "/googleAuth/static/js/runtime-main.7527f68b.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/googleAuth/static/media/crown.ed28accb.svg"
+  }
+]);
